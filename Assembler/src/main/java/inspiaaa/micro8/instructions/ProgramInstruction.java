b2d53c070fcff79556082c3d009d7af906c7a597
Inspiaaa <inspiaaa@gmail.com>
2024-12-08 23:04:20 +0100

    Add instruction address section validation

--- Assembler/src/main/java/inspiaaa/micro8/instructions/ProgramInstruction.java
+++ Assembler/src/main/java/inspiaaa/micro8/instructions/ProgramInstruction.java
@@ -1,8 +1,10 @@
 package inspiaaa.micro8.instructions;
 
+import inspiaaa.assembler.SymbolType;
 import inspiaaa.assembler.memory.AddressContext;
 import inspiaaa.assembler.Instruction;
 import inspiaaa.assembler.SymbolTable;
+import inspiaaa.assembler.memory.MemorySection;
 import inspiaaa.assembler.parser.ErrorReporter;
 
 public class ProgramInstruction extends Instruction {
@@ -12,8 +14,14 @@ public class ProgramInstruction extends Instruction {
 
     @Override
     public void assignAddress(AddressContext context, SymbolTable symtable, ErrorReporter er) {
-        // TODO: Ensure in INSTRUCTION section
         address = context.getAddress();
         context.reserveBits(16);
+    }
+
+    @Override
+    public void validate(SymbolTable symtable, ErrorReporter er) {
+        if (address.getSection() != MemorySection.INSTRUCTION) {
+            er.reportWarning("Writing instruction to data section. Use '.text' to switch to instruction section.", line);
+        }
     }
 }
