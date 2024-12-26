bb8216c0d313cf08dd8a67b17ab8ef9281b6acd1
Inspiaaa <inspiaaa@gmail.com>
2024-12-26 23:38:15 +0100

    Refactor usage of example assembler

---
+++ Assembler/src/main/java/inspiaaa/micro8/Main.java
@@ -0,0 +1,40 @@
+package inspiaaa.micro8;
+
+import inspiaaa.assembler.Assembler;
+import inspiaaa.assembler.memory.Memory;
+import inspiaaa.assembler.memory.MemoryOutputFormat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String path = "examples/sum.S";
+        String code = Files.readString(Path.of(path));
+
+        Assembler assembler = Micro8Assembler.createAssembler(path, code);
+        assembler.assemble();
+        Memory memory = assembler.getMemory();
+
+        System.out.println();
+
+        System.out.println("INSTRUCTION");
+        System.out.println(memory.format(
+                Micro8Assembler.INSTRUCTION_BANK,
+                MemoryOutputFormat.BINARY,
+                16,
+                4,
+                false));
+
+        System.out.println();
+
+        System.out.println("DATA");
+        System.out.println(memory.format(
+                Micro8Assembler.DATA_BANK,
+                MemoryOutputFormat.HEX,
+                2,
+                16,
+                true));
+    }
+}
--- Assembler/src/main/java/inspiaaa/micro8/Micro8Assembler.java
+++ Assembler/src/main/java/inspiaaa/micro8/Micro8Assembler.java
@@ -3,17 +3,12 @@ package inspiaaa.micro8;
 import inspiaaa.assembler.*;
 import inspiaaa.assembler.directives.*;
 import inspiaaa.assembler.memory.MemoryArchitecture;
-import inspiaaa.assembler.memory.MemoryOutputFormat;
 import inspiaaa.assembler.typing.ParameterType;
 import inspiaaa.micro8.directives.CheckedAlignedDataDirective;
 import inspiaaa.micro8.directives.CheckedAsciiDirective;
 import inspiaaa.micro8.directives.CheckedUnalignedDataDirective;
 import inspiaaa.micro8.instructions.*;
 import inspiaaa.micro8.pseudo.*;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class Micro8Assembler {
     public static final String INSTRUCTION_BANK = "instr";
@@ -22,14 +17,12 @@ public class Micro8Assembler {
     public static final String REGISTER = "register";
     public static final ParameterType REGISTER_TYPE = Parameter.distinctNumber(REGISTER);
 
-    private Assembler assembler;
-
-    public Micro8Assembler(String file, String code) {
+    public static Assembler createAssembler(String file, String code) {
         var march = new MemoryArchitecture(INSTRUCTION_BANK);
         march.addBank(INSTRUCTION_BANK, 16, 256);
         march.addBank(DATA_BANK, 8, 256);
 
-        assembler = new Assembler(file, code, march, false);
+        Assembler assembler = new Assembler(file, code, march, false);
 
         assembler.defineConstant("x1", REGISTER, 0);
         assembler.defineConstant("x2", REGISTER, 1);
@@ -64,14 +57,14 @@ public class Micro8Assembler {
 
         assembler.defineInstruction(new VariableDirective(".equ"));
 
-        defineALUInstruction("add", 0);
-        defineALUInstruction("sub", 1);
-        defineALUInstruction("mul", 2);
-        defineALUInstruction("sll", 3);
-        defineALUInstruction("srl", 4);
-        defineALUInstruction("and", 5);
-        defineALUInstruction("or", 6);
-        defineALUInstruction("xor", 7);
+        defineALUInstruction(assembler, "add", 0);
+        defineALUInstruction(assembler, "sub", 1);
+        defineALUInstruction(assembler, "mul", 2);
+        defineALUInstruction(assembler, "sll", 3);
+        defineALUInstruction(assembler, "srl", 4);
+        defineALUInstruction(assembler, "and", 5);
+        defineALUInstruction(assembler, "or", 6);
+        defineALUInstruction(assembler, "xor", 7);
 
         assembler.defineInstruction(new LoadByteInstruction("lb"));
         assembler.defineInstruction(new StoreByteInstruction("sb"));
@@ -96,41 +89,12 @@ public class Micro8Assembler {
         assembler.defineInstruction(new LoadAddressPseudoInstruction("la"));
         assembler.defineInstruction(new BranchPseudoInstruction("bleu", "bgeu"));
         assembler.defineInstruction(new BranchPseudoInstruction("bgtu", "bltu"));
+
+        return assembler;
     }
 
-    private void defineALUInstruction(String name, int opcode) {
+    private static void defineALUInstruction(Assembler assembler, String name, int opcode) {
         assembler.defineInstruction(new ALUInstruction(name, opcode));
         assembler.defineInstruction(new ALUImmInstruction(name + "i", opcode));
-    }
-
-    public static void main(String[] args) throws IOException {
-        // String path = "examples/sum.S";
-        // String path = "examples/fib.S";
-        String path = "examples/example.S";
-        String code = Files.readString(Path.of(path));
-
-        var assembler = new Micro8Assembler(path, code);
-        assembler.assembler.assemble();
-
-        System.out.println();
-
-        System.out.println("INSTRUCTION");
-        System.out.println(assembler.assembler.getMemory().format(
-                INSTRUCTION_BANK,
-                MemoryOutputFormat.BINARY,
-                16,
-                4,
-                false));
-        // System.out.println(assembler.assembler.getMemory().format(MemorySection.INSTRUCTION, false, 4, 16));
-
-        System.out.println();
-
-        System.out.println("DATA");
-        System.out.println(assembler.assembler.getMemory().format(
-                DATA_BANK,
-                MemoryOutputFormat.HEX,
-                2,
-                16,
-                true));
     }
 }
