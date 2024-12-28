293a0f5a300a694296067f5f773211baa3d4c7ec
Inspiaaa <inspiaaa@gmail.com>
2024-12-28 22:14:27 +0100

    Automate output generation in assembler

--- Assembler/src/main/java/inspiaaa/micro8/Main.java
+++ Assembler/src/main/java/inspiaaa/micro8/Main.java
@@ -5,13 +5,20 @@ import inspiaaa.assembler.memory.Memory;
 import inspiaaa.assembler.memory.MemoryOutputFormat;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = "examples/sum_io.S";
-        String code = Files.readString(Path.of(path));
+        String path = "examples/scrolling_image.S";
+
+        Path inputPath = Path.of(path);
+        Path instructionOutputPath = Path.of("examples", "output",
+                inputPath.getFileName().toString() + ".instr.hex");
+        Path dataOutputPath = Path.of("examples", "output",
+                inputPath.getFileName().toString() + ".data.hex");
+        String code = Files.readString(inputPath);
 
         Assembler assembler = Micro8Assembler.createAssembler(path, code);
         assembler.assemble();
@@ -37,13 +44,26 @@ public class Main {
                 16,
                 true));
 
-        System.out.println();
-        System.out.println("v2.0 raw");
-        System.out.println(memory.format(
+        // Write to file.
+
+        PrintWriter outputWriter = new PrintWriter(instructionOutputPath.toString());
+        outputWriter.println("v2.0 raw");
+        outputWriter.println(memory.format(
                 Micro8Assembler.INSTRUCTION_BANK,
                 MemoryOutputFormat.HEX,
                 4,
                 1,
                 true));
+        outputWriter.close();
+
+        outputWriter = new PrintWriter(dataOutputPath.toString());
+        outputWriter.println("v2.0 raw");
+        outputWriter.println(memory.format(
+                Micro8Assembler.DATA_BANK,
+                MemoryOutputFormat.HEX,
+                2,
+                1,
+                true));
+        outputWriter.close();
     }
 }
