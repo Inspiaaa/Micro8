fa87cf8a349a5e0f867bb30fb6da01584a1df0b3
Inspiaaa <inspiaaa@gmail.com>
2025-01-06 01:58:53 +0100

    Remove early validation check in directives
    The previously implemented early validation calls are in fact not necessary and have been removed:
    
    If a symbol that is not defined yet is passed as a parameter, then it will pass the "potentiallyMatches" logic of the parameter checker.
    However, even though the value is accessed during assignAddress (before validate() is called!), this is not a problem.
    
    It does not matter if the symbol receives an incompatible type later, as it does not wait for the symbol to be resolved, since it performs its "work" during assignAddress(). Thus, instead of throwing an internal error due to an incompatible type, it will report a "symbol not found" error.
    
    If the symbol is given an incompatible type before this directive, then it will not pass the parameter type checker.

--- Assembler/src/main/java/inspiaaa/assembler/directives/AlignDirective.java
+++ Assembler/src/main/java/inspiaaa/assembler/directives/AlignDirective.java
@@ -13,17 +13,9 @@ public class AlignDirective extends Instruction {
 
     @Override
     public void assignAddress(InstructionCall instruction, AddressContext context) {
-        // Perform an early type check as this method is called before the regular validate().
-        super.validate(instruction);
         Expr alignmentExpression = instruction.getArguments().get(0);
 
         int alignment = (int)alignmentExpression.getNumericValue();
         context.alignAddress(alignment);
-    }
-
-    @Override
-    public void validate(InstructionCall instruction) {
-        // Nothing to do, as the regular validation already took place during the assignAddress call.
-        // Checking again may produce the same warning again.
     }
 }
